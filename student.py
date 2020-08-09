009bae6fd2d92f8f3f8360bbbe346f60c2f2dfc5
Leo Carnovale <Leo.carnovale@gmail.com>
2020-08-09 13:27:34 +1000

    Better model using improved custom loss function

--- student.py
+++ student.py
@@ -108,7 +108,7 @@ class network(tnn.Module):
     end of the reviews so we can form a batch of reviews of equal length.
     """
     n_in = wordVectors.dim
-    n_hidden = 300
+    n_hidden = 150
     n_layers_lstm = 2
     drop = 0.1
     
@@ -118,29 +118,22 @@ class network(tnn.Module):
             self.n_in, self.n_hidden, self.n_layers_lstm, 
             batch_first=True, bidirectional=False, dropout=self.drop
         )
-        # self.fc0 = tnn.Linear(256*120, 256)
         self.fc1 = tnn.Linear(
-            self.n_hidden * (2 if self.lstm.bidirectional else 1), 300
+            self.n_hidden * (2 if self.lstm.bidirectional else 1), 200
         )
-        self.fc2 = tnn.Linear(300, 50)
+        self.fc2 = tnn.Linear(200, 50)
         self.fc3 = tnn.Linear(50, 5)
-        self.act = tnn.Tanh()
+        self.act = tnn.ReLU()
         self.sm = tnn.Softmax(dim=1)
         self.dropout = tnn.Dropout(self.drop)
 
     def forward(self, iput, length):
         n_b = len(iput)
-        # padded_input = tnn.functional.pad(
-        #     iput, [0, 0, 0, 120 - iput.shape[1]]
-        # ) # has shape [#batches, 120, 50]
         data, states = self.lstm(iput)
         # data = self.dropout(data)
-        data = data[:, -1, :]
-        # data = data.contiguous().view(data.shape[0], -)
-        # data = data.contiguous().view(n_b, 120, -1)
-        # data = self.act(data)
-        # data = data.view(n_b, -1)
-        # data = self.fc0(data)
+        arange = torch.arange(n_b)
+        data = data[arange, length-1, :]
+
         data = self.fc1(data)
         data = self.act(data)
         
@@ -153,25 +146,53 @@ class network(tnn.Module):
         data = self.sm(data)
         return data
 
-class loss(tnn.Module):
+def_dist_weight = 1
+def_err_weight = 2
+class DistLoss(tnn.Module):
     """
     Class for creating a custom loss function, if desired.
     You may remove/comment out this class if you are not using it.
     """
-
-
-    def __init__(self):
-        super(loss, self).__init__()
+    def __init__(self, 
+            dist_weight=def_dist_weight,
+            err_weight=def_err_weight):
+        super(DistLoss, self).__init__()
+        self.dist_weight = dist_weight
+        self.err_weight = err_weight
 
     def forward(self, output, target):
-        dist_weight = 2
-        err_weight = 1
         # targets = convertLabel(target)
         sqr_err = (output - target)**2
         sqr_dist = (output.argmax(dim=1) - target.argmax(dim=1))**2
         sqr_dist = sqr_dist.to(torch.float)
-        mse = err_weight*sqr_err.mean() + dist_weight*sqr_dist.mean()
+        mse = self.err_weight*sqr_err.mean() + self.dist_weight*sqr_dist.mean()
         return mse
+
+    def __str__(self):
+        return "DistLoss(dist_weight=%s, err_weight=%s)" %                     \
+            (self.dist_weight, self.err_weight)
+
+class BCEPlus(tnn.BCELoss):
+    def __init__(self):
+        super(BCEPlus, self).__init__()
+
+    def forward(self, output, target):
+        BCEloss = super(BCEPlus, self).forward(output, target)
+        target_idx = target.argmax(dim=1, keepdims=True)
+        arange = torch.arange(5).repeat([len(target), 1]).to(output.device)
+        dist_from_target = target_idx - arange
+        dist_from_target = (dist_from_target.abs() + 1)**2
+        dist_from_target = dist_from_target.to(torch.float)
+        sqr_err = (output - target)**2
+        product = (dist_from_target * sqr_err)
+        loss = product.mean() + BCEloss
+        return loss
+
+    def __str__(self):
+        return super(BCEPlus, self).__str__() + \
+            " [Uses dist**2 * sqr_err + BCE loss]"
+
+
 
 net = network()
 """
@@ -179,7 +200,8 @@ net = network()
     the torch package, or create your own with the loss class above.
 """
 # lossFunc = tnn.BCELoss()
-lossFunc = loss()
+lossFunc = BCEPlus()
+# lossFunc = DistLoss()
 
 ###########################################################################
 ################ The following determines training options ################
@@ -188,5 +210,5 @@ lossFunc = loss()
 trainValSplit = 0.8
 batchSize = 32
 epochs = 10
-# optimiser = toptim.SGD(net.parameters(), lr=0.02)
+# optimiser = toptim.SGD(net.parameters(), lr=0.03)
 optimiser = toptim.Adam(net.parameters(), lr=0.0001)
